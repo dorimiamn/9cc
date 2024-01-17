57bb7f593c29a72668edd427d4ed834d8b417e9b
dorimiamn <dorimiamn@gmail.com>
2024-01-17 15:09:34 +0900

    エラーメッセージの改良

--- 9cc.c
+++ 9cc.c
@@ -25,12 +25,18 @@ struct Token
 // 現在着目しているトークン
 Token *token;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
-{
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する関数
+void error_at(char *loc, char *fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -52,7 +58,7 @@ void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
     {
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     }
     token = token->next;
 }
@@ -62,7 +68,7 @@ int expect_number()
 {
     if (token->kind != TK_NUM)
     {
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     }
     int val = token->val;
     token = token->next;
@@ -113,7 +119,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(token->str, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -128,8 +134,11 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    // 入力文字列をグローバル変数に保存しておく
+    user_input = argv[1];
+
     // トークナイズする
-    token = tokenize(argv[1]);
+    token = tokenize(user_input);
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
