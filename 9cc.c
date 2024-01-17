4f02e26ee16cc224bc931df92b626108aa4ca48b
dorimiamn <dorimiamn@gmail.com>
2024-01-17 23:46:39 +0900

    単項演算子の処理

--- 9cc.c
+++ 9cc.c
@@ -120,6 +120,7 @@ Node *new_node_num(int val)
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *primary()
@@ -133,15 +134,28 @@ Node *primary()
     return new_node_num(expect_number());
 }
 
+Node *unary()
+{
+    if (consume('+'))
+    {
+        return primary();
+    }
+    if (consume('-'))
+    {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    }
+    return primary();
+}
+
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*')) {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         } else if (consume('/')) {
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         } else {
             return node;
         }
